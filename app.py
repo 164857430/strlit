4eb7c621fc4e745a4c480b72f03e5ce0a398b9cc
CubaRafael <bibo1112@163.com>
2025-12-11 08:57:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ AUTO_ACCESS = os.environ.get('AUTO_ACCESS', 'false').lower() == 'true'
 FILE_PATH = os.environ.get('FILE_PATH', './.cache')
 SUB_PATH = os.environ.get('SUB_PATH', 'sub')
 UUID = os.environ.get('UUID', '6ebc9a9f-5f99-4745-98c6-ebe159299ed9')
-NEZHA_SERVER = os.environ.get('NEZHA_SERVER', 'komar1.netlib.re:443')
+NEZHA_SERVER = os.environ.get('NEZHA_SERVER', 'ooh.pp.ua:443')
 NEZHA_PORT = os.environ.get('NEZHA_PORT', '')
-NEZHA_KEY = os.environ.get('NEZHA_KEY', 'Wk84dOtlkZ4LZQWZGHdk2oSk8F6StczX')
+NEZHA_KEY = os.environ.get('NEZHA_KEY', 'kjdCuPEaZmeCsC6ZHwXgQRIHHgC9qTSQ')
 ARGO_DOMAIN = os.environ.get('ARGO_DOMAIN', 'streamlit.hii.pp.ua')
 ARGO_AUTH = os.environ.get('ARGO_AUTH', 'eyJhIjoiN2JiMzIyOTY2ZTBmZTgzOWY4MTM5MWVkNzZjYWU2YzUiLCJ0IjoiZGNiYTQ5NDUtYTk1My00NDQ1LWEyNzItZGUwZTBiOWZlYzc2IiwicyI6Ik9HTTFNVFF5Wm1RdFltWXlPUzAwTVdGaExXSXlZbVV0WWpNME5tTTNORGd6TWpFMyJ9')
 ARGO_PORT = int(os.environ.get('ARGO_PORT', '8001'))
@@ -622,4 +622,5 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
